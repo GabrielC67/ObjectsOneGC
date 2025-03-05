867f6a82b29872323c8944ee01fde96855d1353a
GabrielC67 <gabecruzpro@gmail.com>
2025-03-05 16:01:20 -0500

    finished

--- object1/src/main/java/object1/Main.java
+++ object1/src/main/java/object1/Main.java
@@ -12,13 +12,18 @@ public class Main {
         System.out.print(person2.name + " ");
         System.out.print(person2.age + " ");
         System.out.println(person2.height);
-        Car vehicle = new Car("Toyota", "Supra", 1997, 69657);
-        System.out.println((vehicle.getYear() + " " + vehicle.getMake() + " " + vehicle.getModel() + " " + vehicle.getMileage()));
+        Car vehicle1 = new Car("Toyota", "Supra", 1997, 69657);
+        Car vehicle2 = new Car("Mazda", "Protege", 1999, 124325);
+        Car vehicle3 = new Car("Acura", "Integra", 1998, 150000);
+
+        System.out.println((vehicle1.getYear() + " " + vehicle1.getMake() + " " + vehicle1.getModel() + " " + vehicle1.getMileage()));
 
         Chair features = new Chair("Cotton", "Green", 8);
         System.out.println(features.getColor() + " " + features.getMaterial() + " chair with " + features.getLegs() + " legs.");
 
         Dog type = new Dog("Cocker Spaniel", 3, "Sporting Dog");
         System.out.println("This " + type.getName()  + ", is a " + type.getBreed() + " that is " + type.getAge() + " years old!");
+
+        System.out.println(vehicle2.getYear() + " " + vehicle2.getMake() + " "  + vehicle2.getModel() + " with " + vehicle2.getMileage());
     }
 }
