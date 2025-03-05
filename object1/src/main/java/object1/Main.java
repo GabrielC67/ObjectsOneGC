59bb6ff93eb9ca71d34dcb5acd4140d22c1c1e02
GabrielC67 <gabecruzpro@gmail.com>
2025-03-05 12:47:14 -0500

    Updated as of 12:47pm

--- object1/src/main/java/object1/Main.java
+++ object1/src/main/java/object1/Main.java
@@ -6,5 +6,10 @@ public class Main {
 
     public static void main(String... args) {
         Logger logger=Logger.getLogger(Main.class.getName());
-        logger.info("This is a module-using Hello World!");    }
+        logger.info("This is a module-using Hello World!");
+        Person person2 = new Person("Gabriel", 39, 167);
+        System.out.print(person2.name + " ");
+        System.out.print(person2.age + " ");
+        System.out.println(person2.height);
+    }
 }
