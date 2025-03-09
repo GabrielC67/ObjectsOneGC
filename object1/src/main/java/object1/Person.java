f995c30d1f22e6609563684ed01c5145b2b8688e
GabrielC67 <gabecruzpro@gmail.com>
2025-03-09 11:12:02 -0400

    I realized that I'm not done yet. Going back to complete.

--- object1/src/main/java/object1/Person.java
+++ object1/src/main/java/object1/Person.java
@@ -18,8 +18,11 @@ class Person {
     public static void main(String[] args) {
         // Declare and initialize an object
         Person person1 = new Person("John", 30, 175);
+        Person person2 = new Person("Gabriel", 39, 167);
 
         /* Now you can use the Object `person1` */
+        System.out.println("Name is: " + person1.name + ". He is " + person1.age + " years old. He is " + person1.height + "cm tall.");
+        System.out.println("Name is: " + person2.name + ". He is " + person2.age + " years old. He is " + person2.height + "cm tall.");
 
     }
 }
